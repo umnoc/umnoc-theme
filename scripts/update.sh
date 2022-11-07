fb8aac915640f446516c26d72d37fb6fe058eee1
MasterGowen <mastergowen@gmail.com>
2022-11-08 01:29:03 +0500

    upd update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,15 @@
-sudo su edxapp -s /bin/bash
+#!/bin/bash
+
+sudo -H -u edxapp bash << EOF
 source /edx/app/edxapp/edxapp_env
 cd ~
 
-cd themes/umnoc-theme/ && git pull
-cd ~ && paver update_assets lms --settings=production --themes umnoc-theme && python manage.py lms --settings=production collectstatic --noinput
+cd themes/umnoc-theme/
+git pull
+cd ~
+
+paver update_assets lms --settings=production
+python manage.py lms --settings=production collectstatic --noinput
+EOF
+
+sudo /edx/bin/supervisorctl restart lms
