e2205345778b24ec565ed095b06a9bbb4a49d02a
MasterGowen <mastergowen@gmail.com>
2024-08-06 14:21:54 +0500

    vscode

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,6 +10,9 @@ cd ~/edx-platform/
 
 paver update_assets lms --themes umnoc-theme --settings=production
 python manage.py lms --settings=production collectstatic --noinput
+
+paver update_assets cms --themes umnoc-theme --settings=production
+python manage.py cms --settings=production collectstatic --noinput
 EOF
 
-sudo /edx/bin/supervisorctl restart lms
+sudo /edx/bin/supervisorctl restart lms cms
